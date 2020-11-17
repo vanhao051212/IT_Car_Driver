cfe2862af2f07eba295c7ef47de5c619c011de32
vanhao051212 <vanhao051212@gmail.com>
2020-11-18 04:09:36 +0700

    first add

---
+++ Driver/project/keil/Objects/core_cm3.d
@@ -0,0 +1,2 @@
+./objects/core_cm3.o: ..\..\lib\CMSIS\CM3\CoreSupport\core_cm3.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ Driver/project/main.c
@@ -0,0 +1,65 @@
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+
+#include <stdio.h>
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+
+
+/* Private function prototypes -----------------------------------------------*/
+#ifdef __GNUC__
+/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/* Private functions ---------------------------------------------------------*/
+
+
+int main(void)
+{
+
+
+  /* Add your application code here
+     */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
